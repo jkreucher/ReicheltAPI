b0e1f5b8f100bca93c3f9fd8338860afad860c65
Jannik Kreucher <linusmueller44@gmx.de>
2025-05-14 23:02:03 +0200

    Fixed Reichelt scraper to adapt to updated website structure

--- README.md
+++ README.md
@@ -9,6 +9,7 @@ A Python module for extracting data from Reichelt Elektronik, a popular German e
 - [Web scraping module for Reichelt Elektronik](#web-scraping-module-for-reichelt-elektronik)
 	- [Table of Contents](#table-of-contents)
 	- [Introduction](#introduction)
+    - [Setup and Dependencies](#setup-and-dependencies)
 	- [Standalone Usage](#standalone-usage)
 	- [Python Quick Start](#python-quick-start)
 	- [Creating the object](#creating-the-object)
@@ -21,30 +22,38 @@ A Python module for extracting data from Reichelt Elektronik, a popular German e
 
 **ReicheltAPI** is a lightweight Python library for scraping data from Reichelt Elektronik using [BeautifulSoup4](https://www.crummy.com/software/BeautifulSoup/). This module aims to assist in generating Bills of Materials (BOMs) and price estimates within KiCad by retrieving information directly from the Reichelt website.
 
+## Setup and Dependencies
+
+Before running the script, create a virtual environment and install the required Python packages:
+```bash
+python3 -m venv .venv
+source .venv/bin/activate
+pip install -r requirements.txt
+```
+
 ## Standalone Usage
 If you just want to query information about a Reichelt part, you can use the module in standalone mode. The command syntax is simple:
 ```bash
 python3 reichelt.py "<ReicheltPartNo>" <attribute>
 ```
-Make sure to enclose the part number in quotes if it contains spaces. The `<attribute>` can be one of the following:
-| Attribute   | Function                                                   |
-| ----------- | ---------------------------------------------------------- |
-| `all`       | everything as json for you to mess around                  |
-| `part`      | shows parsed part number, should be the same as "PartName" |
-| `name`      | Reichelt product name                                      |
-| `link`      | link to the item                                           |
-| `datasheet` | link to the pdf datasheet                                  |
-| `1`         | price of one item                                          |
-| `10`        | price per item when buying 10                              |
-| `100`       | price when ordering 100                                    |
-| `1000`      | price when ordering 1000                                   |
+Make sure to enclose the part number in quotes if it contains spaces. The `<attribute>` is optional and can be any of the keys in the json object. When no attribute is specified the part information will be returned as json string. Here are some useful example attributes:
+| Attribute    | Function                                                   |
+| ------------ | ---------------------------------------------------------- |
+| `part`       | shows parsed part number, should be the same as "PartName" |
+| `name`       | Reichelt product name                                      |
+| `url`        | url to the item                                            |
+| `datasheets` | links to the pdf datasheet                                 |
+| `1`          | price of one item                                          |
+| `10`         | price per item when buying 10                              |
+| `100`        | price when ordering 100                                    |
+| `1000`       | price when ordering 1000                                   |
 
 Examples:
 ```bash
-~$ python3 reichelt.py "Z84C00-06MHZ" name  # returns name of item
-Z80 Microprozessor, 6 MHz, DIP40
-~$ python3 reichelt.py "Z84C00-06MHZ" 1  # return unit price in Euro
-7.25
+~$ python3 reichelt.py "74HC 00" name  # returns name of item
+4-fold, 2 input NAND gates, 2 ... 6 V, DIL-14
+~$ python3 reichelt.py "74HC 00" 1  # return unit price
+0.24
 ```
 
 
@@ -62,52 +71,65 @@ api = reichelt.Reichelt()
 result = api.search_part(part)
 
 # print info
-print(json.dumps(result, indent=4))
+print(json.dumps(result, indent=2))
 
 # download datasheet
-api.get_datasheet(result["datasheet"], part+".pdf")
+api.get_datasheet(result["datasheets"][0], part+".pdf")
 ```
 
 ### Example Output:
 ```json
 {
-    "part": "74HC 00",
-    "name": "4-fold, 2 input NAND gates, 2 ... 6 V, DIL-14",
-    "link": "https://www.reichelt.com/de/en/4-fold-2-input-nand-gates-2--6-v-dil-14-74hc-00-p3119.html?&trstct=pos_1&nbc=1&SID=9433fcf4a460e776488c64328e79f537447333d0260dc8fc87708",
+  "part": "74HC 00",
+  "name": "4-fold, 2 input NAND gates, 2 ... 6 V, DIL-14",
+  "url": "https://www.reichelt.com/de/en/shop/product/4-fold_2_input_nand_gates_2_6_v_dil-14-3119",
+  "price": {
+    "currency": "EUR",
     "1": 0.24,
     "10": null,
     "100": null,
-    "1000": null,
-    "datasheet": "https://www.reichelt.com/index.html?ACTION=7&LA=3&OPEN=0&INDEX=0&FILENAME=A200%2FDS_NXP_74HC00.pdf",
+    "1000": null
+  },
+  "availability": "InStock",
+  "datasheets": [
+    "https://cdn-reichelt.de/documents/datenblatt/A200/DS_NXP_74HC00.pdf",
+    "https://cdn-reichelt.de/documents/datenblatt/A200/DS_STM_M74HC00.pdf",
+    "https://cdn-reichelt.de/documents/datenblatt/A200/U74HC00_UTC.pdf"
+  ],
+  "data": {
     "General": {
-        "family": "74HC",
-        "Model": "NAND-Gate",
-        "Type": "4 inputs",
-        "Design": "4-Elemente",
-        "Mounting form": "DIP-14"
+      "family": "74HC",
+      "Model": "NAND-Gate",
+      "Type": "4 inputs",
+      "Design": "4-Elemente",
+      "Mounting form": "DIP-14"
     },
     "Other": {
-        "Temperature range": "-40 ... +85"
+      "Temperature range": "-40 ... +85"
     },
     "Electrical values": {
-        "Supply voltage": "2.0 ... 6.0 VDC",
-        "Input voltage": "0 ... 6.0 VDC",
-        "Performance": "500 mW",
-        "Input voltage ViH": "4.2 VDC",
-        "Input voltage ViL": "0.5 VDC"
+      "Supply voltage": "2.0 ... 6.0 VDC",
+      "Input voltage": "0 ... 6.0 VDC",
+      "Performance": "500 mW",
+      "Input voltage ViH": "4.2 VDC",
+      "Input voltage ViL": "0.5 VDC"
     },
     "Manufacturer specifications": {
-        "Package weight": "0.001 kg",
-        "RoHS": "conform"
-    },
-    "categories": [
-        "Home",
-        "Components",
-        "Active components",
-        "ICs and controllers",
-        "Digital ICs",
-        "Logic ICs"
-    ]
+      "Manufacturer": "FREI",
+      "Manufacturer ID": "",
+      "Weight": "0.001 kg",
+      "RoHS": "conform",
+      "EAN/GTIN": "9900000031190"
+    }
+  },
+  "categories": [
+    "Home",
+    "Components",
+    "Active components",
+    "ICs and controllers",
+    "Digital ICs",
+    "Logic ICs"
+  ]
 }
 ```
 
@@ -124,34 +146,41 @@ Ensure that the [`reichelt.py`](reichelt.py) file is in the same directory as yo
 
 ## Searching with a string
 
-You can search for a part using the [`get_search_results(keyword)`](reichelt.py) function, which mimics a browser search on the Reichelt homepage but returns the results as a dictionary.
+You can search for a part using the [`get_search_results(keyword)`](reichelt.py) function, which mimics a browser search on the Reichelt homepage and returns the results as a dictionary.
 
 ```python
-result = foo.get_search_results("Z80 CPU")
+results = foo.get_search_results("Z80 CPU")
 ```
 
 ### Example Output
 ```json
 [
-    {
-        "part": "EZ80F91AZA50EK",
-        "name": "8-bit microcontroller, eZ80AcclaimPlus!, LQFP-144",
-        "link": "https://www.reichelt.com/de/en/8-bit-microcontroller-ez80acclaimplus-lqfp-144-ez80f91aza50ek-p376411.html?&trstct=pos_0&nbc=1&SID=968601949b80683845a58c8f12e7374d7421209b4623f36305e19",
-        "1": 17.7,
-        "10": null,
-        "100": null,
-        "1000": null
-    },
-    {
-        "part": "EZ80F92AZ020EG",
-        "name": "8-bit microcontroller, eZ80AcclaimPlus!, LQFP-144",
-        "link": "https://www.reichelt.com/de/en/8-bit-microcontroller-ez80acclaimplus-lqfp-144-ez80f92az020eg-p376413.html?&trstct=pos_1&nbc=1&SID=968601949b80683845a58c8f12e7374d7421209b4623f36305e19",
-        "1": 13.95,
-        "10": null,
-        "100": null,
-        "1000": null
-    },
-...
+  {
+    "part": "EZ80F92AZ020EG",
+    "name": "8-bit microcontroller, eZ80AcclaimPlus!, LQFP-144",
+    "url": "https://www.reichelt.com/de/en/shop/product/8-bit_microcontroller_ez80acclaimplus_lqfp-144-376413",
+    "price": {
+      "1": 13.95,
+      "10": null,
+      "100": null,
+      "1000": null,
+      "currency": "EUR"
+    }
+  },
+  {
+    "part": "EZ80F91AZA50EK",
+    "name": "8-bit microcontroller, eZ80AcclaimPlus!, LQFP-144",
+    "url": "https://www.reichelt.com/de/en/shop/product/8-bit_microcontroller_ez80acclaimplus_lqfp-144-376411",
+    "price": {
+      "1": 17.7,
+      "10": null,
+      "100": null,
+      "1000": null,
+      "currency": "EUR"
+    }
+  },
+  ...
+]
 ```
 
 
@@ -160,55 +189,65 @@ result = foo.get_search_results("Z80 CPU")
 To retrieve detailed information about a specific item, use the [`get_search_results(keyword)`](reichelt.py) function. Pass the URL of the product page to this function.
 
 ```python
-product = foo.get_part_information("https://www.reichelt.de/z80-microprozessor-10-mhz-dip40-z84c00-10mhz-p31823.html?&trstct=pos_2&nbc=1")
+product = foo.get_part_information("https://www.reichelt.com/de/en/shop/product/4-fold_2_input_nand_gates_2_6_v_dil-14-3119")
 ```
 Or, you can directly use the link obtained from [`get_search_results`](reichelt.py):
 ```python
-product = foo.get_part_information(result[0]["link"])
+product = foo.get_part_information(result[0]["url"])
 ```
 
 ```json
 {
-    "datasheet": "https://www.reichelt.com/index.html?ACTION=7&LA=3&OPEN=0&INDEX=0&FILENAME=A200%2FPS0270.pdf",
+  "part": "74HC 00",
+  "name": "4-fold, 2 input NAND gates, 2 ... 6 V, DIL-14",
+  "url": "https://www.reichelt.com/de/en/shop/product/4-fold_2_input_nand_gates_2_6_v_dil-14-3119",
+  "availability": "InStock",
+  "price": {
+    "currency": "EUR",
+    "1": 0.24,
+    "10": null,
+    "100": null,
+    "1000": null
+  },
+  "datasheets": [
+    "https://cdn-reichelt.de/documents/datenblatt/A200/DS_NXP_74HC00.pdf",
+    "https://cdn-reichelt.de/documents/datenblatt/A200/DS_STM_M74HC00.pdf",
+    "https://cdn-reichelt.de/documents/datenblatt/A200/U74HC00_UTC.pdf"
+  ],
+  "data": {
     "General": {
-        "Type": "8-Bit Mikrocontroller",
-        "Technology": "eZ80",
-        "Mounting form": "LQFP-144",
-        "family": "EZ80F91"
-    },
-    "Specials": {
-        "CPU clock": "50 MHz"
-    },
-    "Implementation": {
-        "Memory": "256",
-        "RAM": "8 kByte",
-        "I/O-Pins": "32",
-        "16-bit Timer": "4"
-    },
-    "Interfaces": {
-        "I²C": "1",
-        "UART": "2"
-    },
-    "Electrical values": {
-        "Supply voltage": "3..3.6 VDC"
+      "family": "74HC",
+      "Model": "NAND-Gate",
+      "Type": "4 inputs",
+      "Design": "4-Elemente",
+      "Mounting form": "DIP-14"
     },
     "Other": {
-        "Temperature range": "-40..+105"
+      "Temperature range": "-40 ... +85"
+    },
+    "Electrical values": {
+      "Supply voltage": "2.0 ... 6.0 VDC",
+      "Input voltage": "0 ... 6.0 VDC",
+      "Performance": "500 mW",
+      "Input voltage ViH": "4.2 VDC",
+      "Input voltage ViL": "0.5 VDC"
     },
     "Manufacturer specifications": {
-        "Manufacturer": "ZILOG",
-        "Factory number": "EZ80F91AZA50EK",
-        "Package weight": "0.014 kg",
-        "RoHS": "conform"
-    },
-    "categories": [
-        "Home",
-        "Components",
-        "Active components",
-        "ICs and controllers",
-        "Digital ICs",
-        "8-bit microcontrollers"
-    ]
+      "Manufacturer": "FREI",
+      "Manufacturer ID": "",
+      "Weight": "0.001 kg",
+      "RoHS": "conform",
+      "EAN/GTIN": "9900000031190"
+    }
+  },
+  "categories": [
+    "Home",
+    "Components",
+    "Active components",
+    "ICs and controllers",
+    "Digital ICs",
+    "Logic ICs"
+  ]
 }
 ```
 
@@ -226,6 +265,6 @@ product = reichelt.Reichelt().search_part("74HC 00")
 
 The module also allows you to download the product's datasheet directly from Reichelt. The URL is extracted by the [`get_part_information`](reichelt.py) function, and downloading is as simple as:
 ```python
-foo.get_datasheet(product["datasheet"], "foo.pdf")
+foo.get_datasheet(product["datasheets"][0], "foo.pdf")
 ```
-This will save the datasheet as `foo.pdf` in your current directory.
+This will save the first datasheet as `foo.pdf` in your current directory.
--- reichelt.py
+++ reichelt.py
@@ -1,115 +1,210 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-import urllib.request
 import requests
 from bs4 import BeautifulSoup
 
 
 class Reichelt:
-	def __init__(self):
-		self.req_headers = {
-			'Accept-Language': 'en-US;q=0.7,en;q=0.3'
-		}
-	
+    """
+    Class for interacting with Reichelt Elektronik's product catalog.
 
-	def get_search_results(self, keyword):
-		# download search results as raw html
-		url = "https://www.reichelt.com/index.html?ACTION=446&LA=0&nbc=1&q=%s" % keyword.lower().replace(" ", "%20")
-		website = requests.get(url, headers=self.req_headers).content.decode('utf-8')
-		parser = BeautifulSoup(website, 'html.parser')
-		# get every part in the search result list. Each part is in a div named "al_gallery_article".
-		parts=[]
-		for part_html in parser.find_all("div", "al_gallery_article"):
-			# create json object for each part
-			part={"part":"", "name":"", "link":"", "1":None, "10":None, "100":None, "1000":None}
-			# get link html tag to part
-			part_link = part_html.find("a", "al_artinfo_link")
-			# extract data
-			part["part"] = part_link.contents[0].strip().replace("item-no.: ","") # part number
-			part["name"] = part_link.contents[2].strip() # part name
-			part["link"] = part_link.get("href") # href
-			# find price if there is one
-			if(part_html.find("span", "itemprop")):
-				part["1"] = float(part_html.find("span", "itemprop").next_element.replace(",",".")) # price
-			# find discounts. Price for 10, 100, 1000 peaces are added to the array
-			if(part_html.find("ul", "discounts") ):
-				# get every price from discount table
-				discounts = part_html.find("ul", "discounts").find_all("li")
-				for i in range(len(discounts)):
-					part[str(10**(i+1))] = float(discounts[i].find_all("span")[0].contents[0].replace("€","").replace(",","."))
-			# add part to list
-			parts.append(part)
-		# return part list
-		return parts
-	
+    Provides methods to search for products, extract detailed product data,
+    and download datasheets.
+    """
 
-	def get_part_information(self, url):
-		# download website
-		website = requests.get(url, headers=self.req_headers).content.decode('utf-8')
-		parser = BeautifulSoup(website, "html.parser")
-		# technical information json
-		information = {}
-		# datasheet
-		information["datasheet"] = None
-		if parser.find("div", "av_datasheet"):
-			information["datasheet"] = "https://www.reichelt.com"+parser.find("div", "av_datasheet").a.get("href")
-		# parse information from html
-		information_html = parser.find("div", "av_props_inline")
-		for section_html in information_html.find_all("ul", "av_propview"):
-			section_name = section_html.find("li", "av_propview_headline").contents[0].strip()
-			sections = {}
-			for info in section_html.find_all("ul", "clearfix"):
-				sections[ info.find("li", "av_propname").contents[0].strip() ] =  info.find("li", "av_propvalue").contents[0].strip()
-			information[section_name] = sections
-		# parse categories
-		categories = []
-		category_html = parser.find("ol", "breadcrumb")
-		for category in category_html.find_all("span", itemprop="name"):
-			categories.append( category.contents[0].strip() )
-		information["categories"] = categories
-		# return technical information
-		return information
-	
+    def __init__(self):
+        """
+        Initialize the Reichelt object with default HTTP headers.
+        """
+        self.req_headers = {
+            'Accept-Language': 'en-US;q=0.7,en;q=0.3'
+        }
+    
 
-	def get_datasheet(self, url, filename):
-		data = requests.get(url, headers=self.req_headers).content
-		f = open(filename, "wb")
-		f.write(data)
-		f.close()
-	
+    def get_search_results(self, keyword: str) -> list[dict]:
+        """
+        Search Reichelt for a keyword and return a list of product summaries.
 
-	def search_part(self, part):
-		results = self.get_search_results(part)
-		for result in results:
-			if part == result["part"]:
-				# found right entry
-				result.update( self.get_part_information(result["link"]) )
-				return result
-				
+        Args:
+            keyword (str): Search term for querying products.
+
+        Returns:
+            list[dict]: A list of dictionaries, each containing data about a product (part number, name, price tiers, URL, etc).
+        """
+        # download search results as raw html
+        url = "https://www.reichelt.com/index.html?ACTION=446&LA=0&nbc=1&q=%s" % keyword.lower().replace(" ", "%20")
+        website = requests.get(url, headers=self.req_headers).content.decode('utf-8')
+        parser = BeautifulSoup(website, 'html.parser')
+        # get every part in the search result list. Each part is in a div named "al_gallery_article".
+        parts=[]
+        for part_html in parser.find_all("div", "al_gallery_article"):
+            # create json object for each part
+            part={"part":"", "name":"", "url":"", "price":None}
+            # extract data
+            part["part"] = part_html.find("meta", itemprop="productID").get("content") # part number
+            part["name"] = part_html.find("meta", itemprop="name").get("content") # part name
+            part["url"] = part_html.find("a", "al_artinfo_link").get("href") # href
+            # find price if there is one
+            part["price"] = {}
+            part["price"]["1"] = float(part_html.find("meta", itemprop="price").get("content"))
+            part["price"]["10"] = None
+            part["price"]["100"] = None
+            part["price"]["1000"] = None
+            part["price"]["currency"] = part_html.find("meta", itemprop="priceCurrency").get("content")
+            # find discounts. Price for 10, 100, 1000 peaces are added to the array
+            if(part_html.find("ul", "discounts") ):
+                # get every price from discount table
+                for discount_lis in part_html.find("ul", "discounts").find_all("li"):
+                    span_quant = discount_lis.find("span", attrs={"data-discquant": True})
+                    span_price = discount_lis.find("span", attrs={"data-discprice": True})
+                    quant = span_quant.get("data-discquant") if span_quant else None
+                    price = span_price.get("data-discprice") if span_price else None
+                    if quant:
+                        part["price"][quant] = float(price)
+            # add part to list
+            parts.append(part)
+        # return part list
+        return parts
+    
+
+    def get_part_information(self, url: str) -> dict:
+        """
+        Retrieve detailed information for a specific part from its product page.
+
+        Args:
+            url (str): URL of the product page.
+
+        Returns:
+            dict: A dictionary containing detailed information including name, pricing, availability, datasheets, technical specs, and categories.
+        """
+        # download website
+        website = requests.get(url, headers=self.req_headers).content.decode('utf-8')
+        parser = BeautifulSoup(website, "html.parser")
+        # technical information json
+        information = {}
+        # part number
+        information["part"] = parser.find("span", attrs={"itemprop":"sku"}).find("b").text.strip()
+        information["name"] = parser.find("h1", attrs={"itemprop":"name"}).text.strip()
+        information["url"] = parser.find("meta", attrs={"itemprop":"url"}).get("content").strip()
+        # parse availability
+        information["availability"] = parser.find("link", attrs={"itemprop":"availability"}).get("href").split("/")[-1].strip()
+        # parse price
+        information["price"] = {}
+        information["price"]["currency"] = parser.find("meta", itemprop="priceCurrency").get("content").strip()
+        information["price"]["1"] = float(parser.find("meta", itemprop="price").get("content").strip())
+        information["price"]["10"] = None
+        information["price"]["100"] = None
+        information["price"]["1000"] = None
+        # check if there are discounts
+        price_elements = parser.find_all('p', class_='productPrice right')
+        prices = [float(elem.get_text(strip=True).replace('€', '')) for elem in price_elements]
+        for i, price in enumerate(prices):
+            information["price"][str(10**i)] = price
+        # datasheets
+        information["datasheets"] = []
+        for datasheet_div in parser.find_all("div", attrs={"class": "articleDatasheet"}):
+            information["datasheets"].append(datasheet_div.find("a").get("href"))
+        # part information
+        information["data"] = {}
+        data_html = parser.find_all("ul", attrs={"class":"articleTechnicalData"})
+        for data_category_html in data_html:
+            headline = data_category_html.find("li", "articleTechnicalHeadline").text.strip()
+            information["data"][headline] = {}
+            for data_attribute_html in data_category_html.find_all("ul", "articleAttribute"):
+                data_lis = data_attribute_html.find_all("li")
+                for i in range(0, len(data_lis), 2):
+                    # get attribute name and value
+                    name = data_lis[i].text.strip()
+                    value = data_lis[i+1].text.strip()
+                    information["data"][headline][name] = value
+        # categories
+        information["categories"] = []
+        for category_html in parser.find_all("ol", "breadcrumb"):
+            for category in category_html.find_all("span", itemprop="name"):
+                information["categories"].append(category.contents[0].strip())
+        # return technical information
+        return information
+    
+
+    def get_datasheet(self, url: str, filename: str) -> None:
+        """
+        Download and save a datasheet from a given URL.
+
+        Args:
+            url (str): URL of the datasheet.
+            filename (str): Local filename to save the datasheet.
+        """
+        data = requests.get(url, headers=self.req_headers).content
+        f = open(filename, "wb")
+        f.write(data)
+        f.close()
+    
+
+    def search_part(self, part: str) -> dict:
+        """
+        Search for a specific part number and retrieve full information if found.
+
+        Args:
+            part (str): The part number to search for.
+
+        Returns:
+            dict: A dictionary with full product details if the part is found, else None.
+        """
+        results = self.get_search_results(part)
+        for result in results:
+            if part.upper() == result["part"].upper():
+                # found right entry
+                result.update( self.get_part_information(result["url"]) )
+                return result
+        return None
+                
 
 # stand alone
 if __name__ == '__main__':
-	import sys
-	if len(sys.argv) < 3:
-		print("Usage: ./reichelt.py \"Part Name\" attribute")
-		print("Possible attributes:")
-		print("    all       - print everything for you to parse")
-		print("    part      - parsed part number")
-		print("    name      - Reichelt product name")
-		print("    link      - web link to product")
-		print("    datasheet - link to pdf datasheet")
-		print("    1         - price for one part")
-		print("    10        - discounted price for 10 parts")
-		print("    100       - discounted price for 100 parts")
-		print("    1000      - discounted price for 1000 parts")
-		exit()
-	
-	app = Reichelt()
-	result = app.search_part(sys.argv[1])
-	try:
-		if sys.argv[2] == "all":
-			print(result)
-		else:
-			print(result[sys.argv[2]])
-	except:
-		print("attribute error")
+    import sys
+    import json
+
+    if len(sys.argv) < 2:
+        print("Usage: ./reichelt.py \"Part Name\" optional_attribute")
+        print("The optional attribute can be any key of the jobject returned by the search_part function. Otherwise the whole object is returned.")
+        exit(1)
+    
+    def find_key_recursively(d, target_key):
+        if isinstance(d, dict):
+            for key, value in d.items():
+                if key == target_key:
+                    return value
+                result = find_key_recursively(value, target_key)
+                if result is not None:
+                    return result
+        elif isinstance(d, list):
+            for item in d:
+                result = find_key_recursively(item, target_key)
+                if result is not None:
+                    return result
+        return None
+    
+    app = Reichelt()
+    # search for part
+    result = app.search_part(sys.argv[1])
+    if result is None:
+        print("Part not found")
+        exit(1)
+    # check if there is an optional attribute
+    if len(sys.argv) == 2:
+        print(json.dumps(result, indent=2))
+    else:
+        value = find_key_recursively(result, sys.argv[2])
+        # check type
+        if value is None:
+            print(f"Key \"{sys.argv[2]}\" not found.")
+            exit(1)
+        elif isinstance(value, list):
+            for item in value:
+                print(item)
+        elif isinstance(value, dict):
+            for key, item in value.items():
+                print(f"{key}:{item}")
+        else:
+            print(value)
+    exit(0)
